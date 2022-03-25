10ea5fcb082e37b9601027b344c888329237884b
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-25 15:16:29 +0100

    Added a Register Basic Template

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  register(){
+
+  }
 }
