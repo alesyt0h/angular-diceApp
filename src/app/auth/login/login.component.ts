4fb8ffa75dae50b51af790e12f8e34c9c549e91a
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-25 15:19:49 +0100

    Login working

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { AuthService } from '../../services/auth.service';
+import { AuthResponse } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = this._formBuilder.group({
+    email: ['admin@admin.com', [Validators.required, Validators.email] ],
+    password: ['password', [Validators.required, Validators.minLength(6)]],
+  });
+
+  constructor(private _formBuilder: FormBuilder,
+              private _authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    const {email, password} = this.loginForm.value;
+
+    this._authService.login(email, password)
+    .subscribe((resp: AuthResponse | HttpErrorResponse) => {
+      if (this.isAuthResponse(resp)){
+        console.log(resp.access_token);
+      } else {
+        console.log(resp.error.message);
+      }
+    });
+  }
+
+  isAuthResponse(object: any): object is AuthResponse {
+    return 'access_token' in object;
   }
 
 }
