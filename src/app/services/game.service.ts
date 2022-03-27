ea44ce8e0c4ea804f8c0d6a23f030faebd61d326
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-27 18:19:51 +0200

    Play methods and GameService Set Headers Fix

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -9,32 +9,38 @@ import { ThrowResponse, ThrowsResponse } from '../game/interfaces/throw';
 export class GameService {
 
   apiUrl: string = environment.apiUrl;
-  headers: HttpHeaders = new HttpHeaders()
-          .set('Authorization', 'Bearer ' + this.getToken);
+  headers!: HttpHeaders;
 
   constructor(
     private _http: HttpClient
   ){}
 
   play(id: string){
+    this.setHeaders();
     const url = `${this.apiUrl}/${id}/games`;
 
     return this._http.post<ThrowResponse>(url, {}, {headers: this.headers});
   }
 
   throws(id: number){
+    this.setHeaders();
     const url = `${this.apiUrl}/${id}/games`;
 
     return this._http.get<ThrowsResponse>(url, {headers: this.headers});
   }
 
   get getToken(){
-    return localStorage.getItem('token') || '';
+    return localStorage.getItem('token');
   }
 
   get getUserId(){
     const user = JSON.parse(localStorage.getItem('user') || '{}');
 
     return user.id;
+  }
+
+  setHeaders(){
+    this.headers = new HttpHeaders()
+    .set('Authorization', 'Bearer ' + this.getToken);
   }
 }
