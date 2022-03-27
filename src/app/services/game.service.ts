fd8e93767471380bba00c9195d1b9327cff27ddc
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-27 13:19:47 +0200

    GameService: Play Game Method

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ThrowResponse } from '../game/interfaces/throw';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  apiUrl: string = environment.apiUrl;
+  headers: HttpHeaders = new HttpHeaders()
+  .set('Authorization', 'Bearer ' + this.getToken);
+
+  constructor(
+    private _http: HttpClient
+  ){}
+
+  play(id: string){
+    const url = `${this.apiUrl}/${id}/games`;
+
+    return this._http.post<ThrowResponse>(url, {}, {headers: this.headers});
+  }
+
+  get getToken(){
+    return localStorage.getItem('token') || '';
+  }
+
+  get getUserId(){
+    const user = JSON.parse(localStorage.getItem('user') || '');
+
+    return user.id as string;
+  }
 }
