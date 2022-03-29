31a53a655c7bf84fbde50b6733394cbb5853e8ff
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-29 07:17:13 +0200

    Throws are now with a table and Tailwind pagination

--- src/app/game/throws/throws.component.ts
+++ src/app/game/throws/throws.component.ts
@@ -3,29 +3,32 @@ import { GameService } from '../../services/game.service';
 import { ThrowsResponse, Throw } from '../interfaces/throw';
 
 @Component({
-  selector: 'app-throws',
-  templateUrl: './throws.component.html',
-  styleUrls: ['./throws.component.css']
+    selector: 'app-throws',
+    templateUrl: './throws.component.html',
+    styleUrls: ['./throws.component.css']
 })
 export class ThrowsComponent implements OnInit {
 
-  throws: Throw[] = [];
-  winningPercentage: number = 0;
-  pageOfItems!: Array<any>;
+    throws: Throw[] = [];
+    winningPercentage: number = 0;
+    pageOfItems!: Array<any>;
 
-  constructor(
-    private _gameService: GameService
-  ){}
+    constructor(
+        private _gameService: GameService
+    ){}
 
-  ngOnInit(): void {
-    this._gameService.throws(this._gameService.getUserId).subscribe((resp: ThrowsResponse) => {
-      this.throws = resp.throws;
-      this.winningPercentage = resp.winning_percentage;
-    });
-  }
+    ngOnInit(): void {
+        this._gameService.throws(this._gameService.getUserId).subscribe((resp: ThrowsResponse) => {
+            this.throws = resp.throws;
+            this.winningPercentage = resp.winning_percentage;
+        });
+    }
 
-  onChangePage(pageOfItems: Array<any>) {
-    this.pageOfItems = pageOfItems;
-}
+    onChangePage(pageOfItems: Array<any>) {
+        this.pageOfItems = pageOfItems;
+    }
 
+    getUserId(){
+        return this._gameService.getUserId;
+    }
 }
