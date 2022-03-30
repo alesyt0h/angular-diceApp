fb57096148b319bed21811e7b9072a209bcedabf
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-30 11:17:34 +0200

    Added an AdminGuard

---
+++ src/app/auth/guards/admin.guard.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { map } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminGuard implements CanActivate, CanLoad {
+    constructor(
+        private _router: Router,
+        private _authService: AuthService
+    ) {}
+
+    canActivate(): Observable<boolean> | boolean {
+
+        return this._authService.verify().pipe(
+            map((resp: any) => {
+                console.log(resp)
+                if (this.isHttpErrorResponse(resp) || !resp.admin) {
+                    this._router.navigateByUrl('/play')
+                }
+
+                return true;
+            })
+        );
+    }
+
+    canLoad(): Observable<boolean> | boolean {
+        return this._authService.verify().pipe(
+            map((resp: any) => {
+                console.log(resp)
+
+                if (this.isHttpErrorResponse(resp) || !resp.admin) {
+                    this._router.navigateByUrl('/play')
+                }
+
+                return true;
+            })
+        );
+    }
+
+    isHttpErrorResponse(object: any): object is HttpErrorResponse {
+        return 'error' in object;
+    }
+}
