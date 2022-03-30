5147af1e92fd9d0b120015e1c0a88471936a3a11
Alex Ortigosa <alexortigosa179@gmail.com>
2022-03-30 11:51:03 +0200

    Added a LoggedGuard to prevent going into Auth if the user is logged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedGuard } from './auth/guards/logged.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./game/game.module').then(m => m.GameModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: '**', redirectTo: '404' }
+    { path: '', loadChildren: () => import('./game/game.module').then(m => m.GameModule) },
+    {
+        path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+        canLoad: [LoggedGuard], canActivate: [LoggedGuard]
+    },
+    { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+    { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
